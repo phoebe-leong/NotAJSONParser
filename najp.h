45b90c0d9d810a263f02966e6702034a2bbf2b0f
ph3b3 <phoebeleong8@gmail.com>
2021-09-17 11:00:07 +1000

    console output v2

--- najp.h
+++ najp.h
@@ -1,6 +1,9 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 #include <stdbool.h>
+#include "termcolor-c/termcolor-c.h"
+#include <unistd.h>
 #pragma once
 
 #define NAJP_OK 0
@@ -32,15 +35,31 @@ void najp_open(const char file[], najp* object) {
     object->d.comma = false;
     object->d.objects = 0;
     object->d.parentsubclasses = 0;
+
+    system("clear");
+
+    fputs("  ", stdout);
+    fprintf(text_underline(stdout), "File \"%s\" opened ..\n", file);
+    reset_colors(stdout);
 }
 
 #ifdef __cplusplus
     int najp_addelement(const char title[], const char value[], najp* object) {
-        if (object->d.objects >= NAJP_LIMIT)
+        if (object->d.objects >= NAJP_LIMIT) {
+            sleep(1);
+            fprintf(text_red(stdout), "✗ String element \"%s\" finished with one error: NAJP_ELEMENT_LIMIT_REACHED\n", title);
+            reset_colors(stdout);
+
             return NAJP_ELEMENT_LIMIT_REACHED;
+        }
         for (int i = 0; i != sizeof(object->d.titles) / sizeof(object->d.titles[0]); i++) {
-            if (title == object->d.titles[i])
+            if (title == object->d.titles[i]) {
+                sleep(1);
+                fprintf(text_red(stdout), "✗ String element \"%s\" finished with one error: NAJP_TITLE_ALREADY_IN_USE\n", title);
+                reset_colors(stdout);
+
                 return NAJP_TITLE_ALREADY_IN_USE;
+            }
         }
         object->d.titles[object->d.objects] = title;
         object->d.objects++;
@@ -55,15 +74,29 @@ void najp_open(const char file[], najp* object) {
         }
 
         fprintf(object->json, "\t\"%s\" : \"%s\"", title, value);
+
+        sleep(1);
+        fprintf(text_green(stdout), "✓ String element \"%s\" finished with no errors\n", title);
+        reset_colors(stdout);
         return NAJP_OK;
     }
 
     int najp_addelement(const char title[], int value, najp* object) {
-        if (object->d.objects >= NAJP_LIMIT - 1)
+        if (object->d.objects >= NAJP_LIMIT - 1) {
+            sleep(1);
+            fprintf(text_red(stdout), "✗ Integer element \"%s\" finished with one error: NAJP_ELEMENT_LIMIT_REACHED\n", title);
+            reset_colors(stdout);
+
             return NAJP_ELEMENT_LIMIT_REACHED;
+        }
         for (int i = 0; i != sizeof(object->d.titles) / sizeof(object->d.titles[0]); i++) {
-            if (title == object->d.titles[i])
+            if (title == object->d.titles[i]) {
+                sleep(1);
+                fprintf(text_red(stdout), "✗ Integer element \"%s\" finished with one error: NAJP_TITLE_ALREADY_IN_USE\n", title);
+                reset_colors(stdout);
+
                 return NAJP_TITLE_ALREADY_IN_USE;
+            }
         }
         object->d.titles[object->d.objects] = title;
         object->d.objects++;
@@ -78,15 +111,29 @@ void najp_open(const char file[], najp* object) {
         }
 
             fprintf(object->json, "\t\"%s\" : %d", title, value);
+
+        sleep(1);
+        fprintf(text_green(stdout), "✓ Integer element \"%s\" finished with no errors\n", title);
+        reset_colors(stdout);
         return NAJP_OK;
     }
 
         int najp_addelement(const char title[], bool value, najp* object) {
-        if (object->d.objects >= NAJP_LIMIT)
+        if (object->d.objects >= NAJP_LIMIT) {
+            sleep(1);
+            fprintf(text_red(stdout), "✗ Boolean element \"%s\" finished with one error: NAJP_ELEMENT_LIMIT_REACHED\n", title);
+            reset_colors(stdout);
+
             return NAJP_ELEMENT_LIMIT_REACHED;
+        }
         for (int i = 0; i != sizeof(object->d.titles) / sizeof(object->d.titles[0]); i++) {
-            if (title == object->d.titles[i])
+            if (title == object->d.titles[i]) {
+                sleep(1);
+                fprintf(text_red(stdout), "✗ Boolean element \"%s\" finished with one error: NAJP_TITLE_ALREADY_IN_USE\n", title);
+                reset_colors(stdout);
+
                 return NAJP_TITLE_ALREADY_IN_USE;
+            }
         }
         object->d.titles[object->d.objects] = title;
         object->d.objects++;
@@ -99,18 +146,31 @@ void najp_open(const char file[], najp* object) {
             for (int i = 0; i != object->d.parentsubclasses; i++)
                 fprintf(object->json, "\t");
         }
-
             fprintf(object->json, "\t\"%s\" : ", title);
             fprintf(object->json, value ? "true" : "false");
+
+        sleep(1);
+        fprintf(text_green(stdout), "✓ Boolean element \"%s\" finished with no errors\n", title);
+        reset_colors(stdout);
         return NAJP_OK;
     }
 #else
     int najp_addstrelement(const char title[], const char value[], najp* object) {
-        if (object->d.objects >= NAJP_LIMIT)
+        if (object->d.objects >= NAJP_LIMIT) {
+            sleep(1);
+            fprintf(text_red(stdout), "✗ String element \"%s\" finished with one error: NAJP_ELEMENT_LIMIT_REACHED\n", title);
+            reset_colors(stdout);
+
             return NAJP_ELEMENT_LIMIT_REACHED;
+        }
         for (int i = 0; i != sizeof(object->d.titles) / sizeof(object->d.titles[0]); i++) {
-            if (title == object->d.titles[i])
+            if (title == object->d.titles[i]) {
+                sleep(1);
+                fprintf(text_red(stdout), "✗ String element \"%s\" finished with one error: NAJP_TITLE_ALREADY_IN_USE\n", title);
+                reset_colors(stdout);
+                
                 return NAJP_TITLE_ALREADY_IN_USE;
+            }
         }
         object->d.titles[object->d.objects] = title;
         object->d.objects++;
@@ -118,7 +178,6 @@ void najp_open(const char file[], najp* object) {
             fprintf(object->json, ",\n");
         if (object->d.isubclass) {
             object->d.isubclasstart = false;
-
             for (int i = 0; i != object->d.parentsubclasses; i++)
                 fprintf(object->json, "\t");
         }
@@ -127,15 +186,29 @@ void najp_open(const char file[], najp* object) {
             object->d.comma = true;
 
         fprintf(object->json, "\t\"%s\" : \"%s\"", title, value);
+
+        sleep(1);
+        fprintf(text_green(stdout), "✓ String element \"%s\" finished with no errors\n", title);
+        reset_colors(stdout);
         return NAJP_OK;
     }
 
     int najp_addnumelement(const char title[], const int value, najp* object) {
-        if (object->d.objects >= NAJP_LIMIT)
+        if (object->d.objects >= NAJP_LIMIT) {
+            sleep(1);
+            fprintf(text_red(stdout), "✗ Integer element \"%s\" finished with one error: NAJP_ELEMENT_LIMIT_REACHED\n", title);
+            reset_colors(stdout);
+
             return NAJP_ELEMENT_LIMIT_REACHED;
+        }
         for (int i = 0; i != sizeof(object->d.titles) / sizeof(object->d.titles[0]); i++) {
-            if (title == object->d.titles[i])
+            if (title == object->d.titles[i]) {
+                sleep(1);
+                fprintf(text_red(stdout), "✗ Integer element \"%s\" finished with one error: NAJP_TITLE_ALREADY_IN_USE\n", title);
+                reset_colors(stdout);
+
                 return NAJP_TITLE_ALREADY_IN_USE;
+            }
         }
         object->d.titles[object->d.objects] = title;
         object->d.objects++;
@@ -143,7 +216,6 @@ void najp_open(const char file[], najp* object) {
             fprintf(object->json, ",\n");
         if (object->d.isubclass) {
             object->d.isubclasstart = false;
-
             for (int i = 0; i != object->d.parentsubclasses; i++)
                 fprintf(object->json, "\t");
         }
@@ -152,15 +224,29 @@ void najp_open(const char file[], najp* object) {
             object->d.comma = true;
 
         fprintf(object->json, "\t\"%s\" : %d", title, value);
+
+        sleep(1);
+        fprintf(text_green(stdout), "✓ Integer element \"%s\" finished with no errors\n", title);
+        reset_colors(stdout);
         return NAJP_OK;
     }
 
     int najp_addboolelement(const char title[], const bool value, najp* object) {
-        if (object->d.objects >= NAJP_LIMIT)
+        if (object->d.objects >= NAJP_LIMIT) {
+            sleep(1);
+            fprintf(text_red(stdout), "✗ Boolean element \"%s\" finished with one error: NAJP_ELEMENT_LIMIT_REACHED\n", title);
+            reset_colors(stdout);
+
             return NAJP_ELEMENT_LIMIT_REACHED;
+        }
         for (int i = 0; i != sizeof(object->d.titles) / sizeof(object->d.titles[0]); i++) {
-            if (title == object->d.titles[i])
+            if (title == object->d.titles[i]) {
+                sleep(1);
+                fprintf(text_red(stdout), "✗ Boolean element \"%s\" finished with one error: NAJP_TITLE_ALREADY_IN_USE\n", title);
+                reset_colors(stdout);
+
                 return NAJP_TITLE_ALREADY_IN_USE;
+            }
         }
         object->d.titles[object->d.objects] = title;
         object->d.objects++;
@@ -177,14 +263,23 @@ void najp_open(const char file[], najp* object) {
 
         fprintf(object->json, "\t\"%s\" : ", title);
         fprintf(object->json, value ? "true" : "false");
+
+        sleep(1);
+        fprintf(text_green(stdout), "✓ Boolean element \"%s\" finished with no errors\n", title);
+        reset_colors(stdout);
         return NAJP_OK;
     }
 #endif
 
 int najp_addarray(const char title[], najp_array values[], const size_t valuesamt, najp* object) {
     for (int i = 0; i != sizeof(object->d.titles) / sizeof(object->d.titles[0]); i++) {
-        if (title == object->d.titles[i])
+        if (title == object->d.titles[i]) {
+            sleep(1);
+            fprintf(text_red(stdout), "✗ Array \"%s\" finished with one error: NAJP_TITLE_ALREADY_IN_USE\n", title);
+            reset_colors(stdout);
+
             return NAJP_TITLE_ALREADY_IN_USE;
+        }
     }
     object->d.titles[object->d.objects] = title;
     object->d.objects++;
@@ -215,13 +310,22 @@ int najp_addarray(const char title[], najp_array values[], const size_t valuesam
         fprintf(object->json, "\n\t\t]");
     else
         fprintf(object->json, "\n\t]");
+
+    sleep(1);
+    fprintf(text_green(stdout), "✓ Array \"%s\" finished with no errors\n", title);
+    reset_colors(stdout);
     return NAJP_OK;
 }
 
 int najp_addsubclass(const char title[], najp* object) {
     for (int i = 0; i != sizeof(object->d.titles) / sizeof(object->d.titles[0]); i++)
-        if (title == object->d.titles[i])
+        if (title == object->d.titles[i]) {
+            sleep(1);
+            fprintf(text_red(stdout), "✗ Subclass \"%s\" finished with one error: NAJP_TITLE_ALREADY_IN_USE\n", title);
+            reset_colors(stdout);
+
             return NAJP_TITLE_ALREADY_IN_USE;
+        }
     if (object->d.comma)
         fprintf(object->json, ",\n");
     if (object->d.parentsubclasses != 0)
@@ -233,12 +337,16 @@ int najp_addsubclass(const char title[], najp* object) {
     object->d.isubclass = true;
     object->d.isubclasstart = true;
     object->d.comma = false;
+
+    sleep(1);
+    fprintf(text_green(stdout), "✓ Subclass \"%s\" opened with no errors\n", title);
+    reset_colors(stdout);
     return NAJP_OK;
 }
 
 void najp_closesubclass(najp* object) {
     if (!object->d.isubclasstart)
-        fprintf(object->json, "\n");
+        fprintf(object->json, "\n"); 
     for (int i = 0; i < object->d.parentsubclasses - 1; i++)
         fprintf(object->json, "\t");
     fprintf(object->json, "\t}");
@@ -250,4 +358,10 @@ void najp_closesubclass(najp* object) {
 void najp_close(const najp* object) {
     fprintf(object->json, "\n}");
     fclose(object->json);
+
+    sleep(1);
+    fputs("  ", stdout);
+    fputs("File closed ..\n", text_underline(stdout));
+    reset_colors(stdout);
+    sleep(1);
 }
